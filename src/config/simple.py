eb037123c2bfd598175c2282844965e2c2fc77ae
Olga Kozlova <grenlaykk@gmail.com>
2022-02-24 01:09:41 +0300

    Update storage path

--- src/config/simple.py
+++ src/config/simple.py
@@ -1,7 +1,7 @@
 from training.simple import SimpleTrainer
 from src.models.simple import Model
 from src.models.simple import Loss
-from src.logger.storage import Storage
+from src.logger.storage.storage import Storage
 from src.logger.simple import Logger
 from src.data.simple import SimpleDataset
 from torch.utils.data import DataLoader
@@ -11,11 +11,11 @@ class Config:
         model = Model()
         criterion = Loss()
         optimizer = None
-        storage = Storage()
+        storage = Storage('path/to/storage')
         logger = Logger()
         train_dataloader = DataLoader(SimpleDataset('local/train'))
         val_dataloader = DataLoader(SimpleDataset('local/val'))
-        
+
         self.trainer = SimpleTrainer(model, criterion, optimizer, storage, 
                                      logger, train_dataloader, val_dataloader)
 
