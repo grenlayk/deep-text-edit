b1e7c9ba4393d70bc66972a66925b4ce46cf1f0e
nikoryagin <nskoryagin@gmail.com>
2022-02-25 21:09:01 +0300

    Fix bugs, add end_val

--- src/logger/simple.py
+++ src/logger/simple.py
@@ -1,94 +1,107 @@
+from __future__ import annotations
+from collections import defaultdict
 import time
 from torch import Tensor
 from loguru import logger
-from __future__ import annotations
 
 class Logger():
-    def __init__(self, print_freq: int = 100, image_freq: int = 1000, val_size: int = None, tb_path: str = None):
+    def __init__(self, print_freq: int = 100, image_freq: int = 1000, tb_path: str = None):
         self.print_freq: int = print_freq
         self.image_freq: int = image_freq
-        self.loss_buff: dict[str, list[float]] = {}
+        self.loss_buff: dict[str, dict[str, list]] = defaultdict()
+        self.loss_buff['values'] = defaultdict(list)
+        self.loss_buff['sumlast'] = defaultdict(int)
+        self.loss_buff['sum'] = defaultdict(int)
+        self.metrics_buff:dict[str, dict[str, list]] = defaultdict()
+        self.metrics_buff['values'] = defaultdict(list)
+        self.metrics_buff['sumlast'] = defaultdict(int)
+        self.metrics_buff['sum'] = defaultdict(int)
         self.tb_path: str = tb_path
         self.train_iter = 1
         self.val_iter = 1
-        # сомнительно 
-        self.val_size = val_size # in batches
 
-    def log_train(self, losses: dict[str, float], images: dict[str, Tensor]):
+
+    def log_train(self, losses: dict[str, float] = {}, images: dict[str, Tensor] = {}):
         if self.train_iter == 1:
             self.start_time = time.time()
             logger.info(
                     'Training started'
                 )
 
-        for loss_name, loss_value in losses:
-            self.loss_buff[loss_name] += [loss_value]
-            self.loss_buff[loss_name + ' sum'] += loss_value
+        for loss_name, loss_value in losses.items():
+            self.loss_buff['values'][loss_name] += [loss_value]
+            self.loss_buff['sumlast'][loss_name] += loss_value
         
         if self.train_iter % self.print_freq == 0:
             self.end_time = time.time()
-            logger.info(
-                    f'Batch: {self.train_iter}',
-                    f'Processing time for last {self.print_freq} batches: {self.end_time - self.end_time:.3f}s',
-                    *[f'Average {loss_name} over last {self.print_freq} batches: {self.loss_buff[loss_name + " sum"] / self.print_freq}' \
-                        for loss_name in losses]
-                )
+            if losses:
+                logger.info(f'Batch: {self.train_iter}')
+                logger.info(f'Processing time for last {self.print_freq} batches: {self.end_time - self.end_time:.3f}s')
+                for loss_name in losses:
+                    logger.info(f'Average {loss_name} over last {self.print_freq} batches: {self.loss_buff["sumlast"][loss_name] / self.print_freq}')
+                logger.info('------------')
             self.start_time = self.end_time
-            self.loss_buff.clear()
+            self.loss_buff['values'].clear()
+            self.loss_buff['sumlast'].clear()
         
-        if self.train_iter % self.image_freq == 0:
+        if self.train_iter % self.image_freq == 0 and images:
             #TensorBoard magic
             pass
 
         self.train_iter += 1
 
-    def log_val(self, losses: dict[str, float], images: dict[str, Tensor], metrics: dict[str, float]):
+    def log_val(self, losses: dict[str, float] = {}, images: dict[str, Tensor] = {}, metrics: dict[str, float] = {}):
         
         if self.val_iter == 1:
-            self.metrics_buff = {}
-            self.loss_buff.clear()
+            self.loss_buff['values'].clear()
+            self.loss_buff['sumlast'].clear()
             logger.info(
                     'Validation started'
                 )
 
-        for loss_name, loss_value in losses:
-            self.loss_buff[loss_name] += [loss_value]
-            self.loss_buff[loss_name + ' sum'] += loss_value
-            self.loss_buff[loss_name + ' sumlast'] += loss_value # сомнительное решение
+        for loss_name, loss_value in losses.items():
+            self.loss_buff['values'][loss_name] += [loss_value]
+            self.loss_buff['sum'][loss_name] += loss_value
+            self.loss_buff['sumlast'][loss_name] += loss_value
 
 
-        for metric_name, metric_value in metrics:
-            self.metrics_buff[metric_name] += [metric_value]
-            self.metrics_buff[metric_name + ' sum'] += metric_value
-            self.metrics_buff[metric_name + ' sumlast'] += metric_value # сомнительное решение
+        for metric_name, metric_value in metrics.items():
+            self.metrics_buff['values'][metric_name] += [metric_value]
+            self.metrics_buff['sum'][metric_name] += metric_value
+            self.metrics_buff['sumlast'][metric_name] += metric_value
 
 
         if self.val_iter % self.print_freq == 0:
-            logger.info(
-                    f'Batch: {self.val_iter}',
-                    *[f'Average {loss_name} over last {self.print_freq} batches: {self.loss_buff[loss_name + " sumlast"] / self.print_freq}' \
-                        for loss_name in metrics],
-                    *[f'Average {metric_name} over last {self.print_freq} batches: {self.metrics_buff[metric_name + " sumlast"] / self.print_freq}' \
-                        for metric_name in losses]
-                )
-            # следствие сомнительного решения
-            for loss_name in losses:
-                self.loss_buff[loss_name + ' sumlast'] = 0
-            for metric_name in metrics:
-                self.metrics_buff[metric_name + ' sumlast'] = 0
+            if losses:
+                logger.info(f'Batch: {self.val_iter}')
+                for loss_name in losses:
+                    logger.info(f'Average {loss_name} over last {self.print_freq} batches: {self.loss_buff["sumlast"][loss_name] / self.print_freq}')
+                self.loss_buff['values'].clear()
+                self.loss_buff['sumlast'].clear()
 
-        if self.val_iter == self.val_size:
-            logger.info(
-                    *[f'{loss_name}: {self.loss_buff[loss_name + " sum"] / self.val_size}' \
-                        for loss_name in losses]
-                )
-
-            logger.info(
-                    *[f'{metric_name}: {self.loss_buff[metric_name + " sum"] / self.val_size}' \
-                        for metric_name in metrics]
-                )
+            if metrics:
+                for metric_name in metrics:
+                    logger.info(f'Average {metric_name} over last {self.print_freq} batches: {self.metrics_buff["sumlast"][metric_name] / self.print_freq}')
+                self.metrics_buff['values'].clear()
+                self.metrics_buff['sumlast'].clear()
+            logger.info('------------')
 
 
         if self.val_iter % self.image_freq == 0:
             #TensorBoard magic
             pass
+
+        self.val_iter += 1
+
+
+    def end_val(self):
+        if self.loss_buff:
+            for loss_name in self.loss_buff['sum']:
+                logger.info(f'Average {loss_name} over validation: {self.loss_buff["sum"][loss_name] / (self.val_iter - 1)}')
+        if self.metrics_buff:
+            for metric_name in self.metrics_buff['sum']:
+                logger.info(f'Average {metric_name} over validation: {self.metrics_buff["sum"][metric_name] / (self.val_iter - 1)}')
+        self.loss_buff.clear()
+        self.metrics_buff.clear()
+        self.val_iter = 1
+        self.train_iter = 1
