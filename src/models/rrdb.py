de47dadc038ed14a933896320e3f273da514e5dd
nikoryagin <nskoryagin@gmail.com>
2022-03-31 14:42:41 +0300

    Edit rrdb size

--- src/models/rrdb.py
+++ src/models/rrdb.py
@@ -80,7 +80,7 @@ class RRDBNet(nn.Module):
 class RRDB_pretrained(nn.Module):
     def __init__(self, model_path = None):
         super().__init__()
-        self.model = RRDBNet(6, 3, 64, 23, gc=32)
+        self.model = RRDBNet(6, 3, 64, 10, gc=32)
         if model_path:
             self.model.load_state_dict(torch.load(model_path), strict=True)
     def forward(self, x):
