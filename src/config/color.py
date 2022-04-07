0eb73246246a2b77c243b011dd7a0f2d02a97bd7
Alexander Bogdanov <a@bogdanov.co>
2022-04-07 10:43:36 +0300

    Use RRDB block in colorization

--- src/config/color.py
+++ src/config/color.py
@@ -6,7 +6,7 @@ from loguru import logger
 from src.data.color import CustomDataset
 from src.disk import disk
 from src.logger.simple import Logger
-from src.models.color import Model
+from src.models.rrdb import RRDBNet
 from src.storage.simple import Storage
 from src.training.color import ColorizationTrainer
 
@@ -17,23 +17,26 @@ class Config:
         device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
         logger.info(f'Using device: {device}')
 
-        if not Path('data/miniCoco').exists():
-            disk.download('data/miniCoco.zip', 'data/miniCoco.zip')
-            with zipfile.ZipFile('data/miniCoco.zip', 'r') as zip_ref:
+        data_path = Path('data/Coco')
+
+        if not data_path.exists():
+            disk.download(data_path.with_suffix('.zip'), data_path.with_suffix('.zip'))
+            with zipfile.ZipFile(data_path.with_suffix('.zip'), 'r') as zip_ref:
                 zip_ref.extractall('data/')
 
-        total_epochs = 3 #20
-        model = Model(256, device).to(device)
+        total_epochs = 20  # 20
+        model = RRDBNet(3, 3, 64, 10, gc=32).to(device)
         criterion = torch.nn.MSELoss(reduction='mean').to(device)
-        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-6)
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-6)
         scheduler = torch.optim.lr_scheduler.MultiStepLR(
             optimizer,
             milestones=list(range(0, total_epochs, 5)),
-            gamma=0.2
+            gamma=0.2,
+            verbose=True
         )
 
-        batch_size = 6
-        train_dataset = CustomDataset(Path('./data/raw/miniCoco/train'))
+        batch_size = 56
+        train_dataset = CustomDataset(data_path / 'train', training=True)
         train_dataloader = torch.utils.data.DataLoader(
             train_dataset,
             batch_size=batch_size,
@@ -41,7 +44,7 @@ class Config:
             num_workers=8
         )
 
-        val_dataset = CustomDataset(Path('./data/raw/miniCoco/val'))
+        val_dataset = CustomDataset(data_path / 'val')
         val_dataloader = torch.utils.data.DataLoader(
             val_dataset,
             batch_size=batch_size,
@@ -49,7 +52,7 @@ class Config:
             num_workers=8
         )
 
-        test_dataset = CustomDataset(Path('./data/raw/miniCoco/test'))
+        test_dataset = CustomDataset(data_path / 'test')
         test_dataloader = torch.utils.data.DataLoader(
             test_dataset,
             batch_size=1,
@@ -57,8 +60,8 @@ class Config:
             num_workers=8
         )
 
-        metric_logger = Logger(print_freq=2, image_freq=3, project_name='colorization')
-        storage = Storage('./checkpoints/colorization')
+        metric_logger = Logger(print_freq=100, image_freq=100, project_name='colorization_rrdb')
+        storage = Storage('./checkpoints/colorization_rrdb')
 
         self.trainer = ColorizationTrainer(
             model,
