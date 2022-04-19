e896404d7ab2c929b21cdb3cb5de0d72a1a4d4f0
Alexander Bogdanov <a@bogdanov.co>
2022-04-20 00:09:21 +0300

    ✨ Add warmup scheduler

--- src/data/color.py
+++ src/data/color.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Optional, Tuple, Union
 
 import cv2
 import torch
@@ -9,10 +10,20 @@ from torchvision import transforms as T
 
 
 class CustomDataset(Dataset):
-    def __init__(self, root_dir: Path, training: bool = False):
+    def __init__(self, root_dir: Path, crop_size: Optional[Union[Tuple, int]] = None):
         self.root_dir = root_dir
         self.files = list(root_dir.iterdir())
-        self.training = training
+
+        transforms = [
+            T.Lambda(lambda img: cv2.cvtColor(img, cv2.COLOR_BGR2RGB)),
+            T.ToTensor()
+        ]
+
+        if crop_size is not None:
+            transforms.append(T.RandomCrop(crop_size))
+
+        self.transform = T.Compose(transforms)
+
         logger.info(f'File[0]: {self.files[0]}, Total Files: {len(self.files)}')
 
     def __len__(self):
@@ -21,10 +32,7 @@ class CustomDataset(Dataset):
     @logger.catch
     def __getitem__(self, index: int):
         try:
-            rgb_img = T.ToTensor()(cv2.imread(str(self.files[index])))
-            if rgb_img is None:
-                raise Exception
-            rgb_img = T.RandomCrop(32)(rgb_img)
+            rgb_img = self.transform(cv2.imread(str(self.files[index])))
 
             bw_img = TF.adjust_saturation(rgb_img, 0)
 
