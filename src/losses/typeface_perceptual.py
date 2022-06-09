92a33e05e3d186979d640cbdf0cc3572c0d94e13
nikoryagin <nskoryagin@gmail.com>
2022-06-10 02:58:06 +0300

    new exp

--- src/losses/typeface_perceptual.py
+++ src/losses/typeface_perceptual.py
@@ -1,51 +1,31 @@
 import torch
 import torchvision
-from src.disk import disk 
+import torchvision.transforms as T
+from src.disk import disk
 from pathlib import Path
+
+
 class TypefacePerceptualLoss(torch.nn.Module):
-    def __init__(self, model_remote_path = 'checkpoints/typeface/2/model', model_local_path = 'typeface', resize=True):
-        super(TypefacePerceptualLoss, self).__init__()
+    def __init__(
+            self,
+            model_remote_path='models/TypefaceClassifier/model',
+            model_local_path='models/TypefaceClassifier/model'):
+        super().__init__()
         if not Path(model_local_path).exists():
             disk.download(model_remote_path, model_local_path)
-        
-        model = torchvision.models.vgg11()
+
+        model = torchvision.models.vgg16().cuda()
         model.classifier[-1] = torch.nn.Linear(4096, 2500)
         model.load_state_dict(torch.load(model_local_path))
-        blocks = []
-        blocks.append(model.features[:5].eval())
-        blocks.append(model.features[5:10].eval())
-        blocks.append(model.features[10:15].eval())
-        blocks.append(model.features[15:20].eval())
-        for bl in blocks:
-            for p in bl.parameters():
-                p.requires_grad = False
-        self.blocks = torch.nn.ModuleList(blocks)
-        self.transform = torch.nn.functional.interpolate
-        self.resize = resize
-        self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
-        self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
+        model.classifier[-1] = torch.nn.Identity()
+        self.model = model.eval()
+        for p in model.parameters():
+            p.requires_grad = False
 
-    def forward(self, input, target, feature_layers=[0, 1, 2, 3], style_layers=[]):
-        if input.shape[1] != 3:
-            input = input.repeat(1, 3, 1, 1)
-            target = target.repeat(1, 3, 1, 1)
-        input = (input-self.mean) / self.std
-        target = (target-self.mean) / self.std
-        if self.resize:
-            input = self.transform(input, mode='bilinear', size=(64, 196), align_corners=False)
-            target = self.transform(target, mode='bilinear', size=(64, 196), align_corners=False)
-        loss = 0.0
-        x = input
-        y = target
-        for i, block in enumerate(self.blocks):
-            x = block(x)
-            y = block(y)
-            if i in feature_layers:
-                loss += torch.nn.functional.l1_loss(x, y)
-            if i in style_layers:
-                act_x = x.reshape(x.shape[0], x.shape[1], -1)
-                act_y = y.reshape(y.shape[0], y.shape[1], -1)
-                gram_x = act_x @ act_x.permute(0, 2, 1)
-                gram_y = act_y @ act_y.permute(0, 2, 1)
-                loss += torch.nn.functional.l1_loss(gram_x, gram_y)
-        return loss
+        self.transform = T.Compose([
+            T.Resize((64, 192)),
+            T.Normalize([0.4803, 0.4481, 0.3976], [0.2769, 0.2690, 0.2820]),
+        ])
+
+    def forward(self, inputs, targets):
+        return torch.nn.functional.l1_loss(self.model(inputs), self.model(targets))
---
+++ src/losses/vgg.py
@@ -0,0 +1,57 @@
+import torch
+import torchvision
+from torchvision import transforms as T
+
+
+class VGGLoss(torch.nn.Module):
+    def __init__(self, perc_layers=None, tex_layers=None):
+        super().__init__()
+
+        if perc_layers is None:
+            perc_layers = [0, 1, 2, 3, 4]
+        if tex_layers is None:
+            tex_layers = [0, 1, 2, 3, 4]
+
+        model = torchvision.models.vgg16(pretrained=True)
+        vgg_layers = list(model.features.children())
+        blocks = [
+            torch.nn.Sequential(*vgg_layers[:4]).eval(),
+            torch.nn.Sequential(*vgg_layers[4:9]).eval(),
+            torch.nn.Sequential(*vgg_layers[9:16]).eval(),
+            torch.nn.Sequential(*vgg_layers[16:23]).eval(),
+            torch.nn.Sequential(*vgg_layers[23:30]).eval()
+        ]
+        for bl in blocks:
+            for p in bl.parameters():
+                p.requires_grad = False
+        self.blocks = torch.nn.ModuleList(blocks)
+
+        self.transform = torch.nn.functional.interpolate
+        self.transform = T.Compose([
+            T.Resize((224, 224)),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        self.tex_layers = tex_layers
+        self.perc_layers = perc_layers
+
+    def forward(self, inputs, target):
+        if inputs.shape[1] != 3:
+            inputs = inputs.repeat(1, 3, 1, 1)
+            target = target.repeat(1, 3, 1, 1)
+        perc_loss = 0.0
+        gram_loss = 0.0
+        x = inputs
+        y = target
+        for i, block in enumerate(self.blocks):
+            x = block(x)
+            y = block(y)
+            _, _, h, w = x.shape
+            if i in self.perc_layers:
+                perc_loss += torch.nn.functional.l1_loss(x, y) / (h * w) / len(self.perc_layers)
+            if i in self.tex_layers:
+                act_x = x.reshape(x.shape[0], x.shape[1], -1)
+                act_y = y.reshape(y.shape[0], y.shape[1], -1)
+                gram_x = act_x @ act_x.permute(0, 2, 1)
+                gram_y = act_y @ act_y.permute(0, 2, 1)
+                gram_loss += torch.nn.functional.l1_loss(gram_x, gram_y) / (h * w) / len(self.tex_layers)
+        return perc_loss, gram_loss
