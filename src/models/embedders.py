c9cb8871d6b277346a15332aa4e0297b735fb1e5
Nikita Koryagin <61164367+nikoryagin@users.noreply.github.com>
2022-06-18 20:27:05 +0300

    Fix init of style_embedder

--- src/models/embedders.py
+++ src/models/embedders.py
@@ -6,6 +6,7 @@ class ContentResnet(models.ResNet):
     def __init__(self):
         # resnet18 init
         super().__init__(BasicBlock, [2, 2, 2, 2])
+        self.fc = torch.nn.Identity()
 
     def _forward_impl(self, x):
         # See note [TorchScript super()]
