b779d4c8b49ee9420cacb5790554fcc263b765ed
Olga Kozlova <grenlayk@yandex.ru>
2022-06-19 11:43:49 +0300

    Delete resnet forward

--- src/models/embedders.py
+++ src/models/embedders.py
@@ -31,21 +31,3 @@ class StyleResnet(models.ResNet):
         # resnet18 init
         super().__init__(BasicBlock, [2, 2, 2, 2])
         self.fc = torch.nn.Identity()
-
-    def _forward_impl(self, x):
-        # See note [TorchScript super()]
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        #x = self.fc(x)
-
-        return x
