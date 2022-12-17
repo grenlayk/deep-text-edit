d4a5c257b5c52a520d4563f2d18b011ad945ea0f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-17 20:52:22 +0000

    Adding tarfile member sanitization to extractall()

--- src/config/typeface.py
+++ src/config/typeface.py
@@ -26,7 +26,29 @@ class Config:
         if not Path('data/Typefaces').exists():
             disk.download('data/Typefaces.tar', 'data/Typefaces.tar')
             with tarfile.open('data/Typefaces.tar', 'r') as tar:
-                tar.extractall('data/Typefaces')
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, "data/Typefaces")
 
         model = vgg11(pretrained=True)
         model.classifier[-1] = torch.nn.Linear(4096, 2500)
