14f1867af96fcad278656cc1ab3277dd7f088ffd
Your Name <you@example.com>
2023-09-29 13:17:28 +0200

    image source

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import cv2
 from src.data.baseline import ImgurDataset
-from src.data.wrappers import DrawText, ChannelShuffleImage, Resize
+from src.data.wrappers import DrawText, ChannelShuffleImage, Resize, NormalizeImages
 
 size = (192, 64)
 root = Path("")
@@ -11,4 +11,5 @@ dataset = ChannelShuffleImage(dataset, 'image')
 dataset = DrawText(dataset, 'content', 'draw')
 dataset = Resize(dataset, 'image', size)
 dataset = Resize(dataset, 'draw', size, interpolation=cv2.INTER_NEAREST)
+dataset = NormalizeImages(dataset, ['image', 'draw'])
 
