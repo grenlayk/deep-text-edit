63bec7f1e0a955c74d5850f6628a7549c850d022
Renat Khizbullin <renat.khizbullin@incode.com>
2023-10-05 10:43:04 +0200

    Add gram3

---
+++ src/config/pl/simplest_typeface.py
@@ -0,0 +1,99 @@
+from pathlib import Path
+
+import cv2
+import torch
+import torchmetrics.image
+from pytorch_lightning import Trainer
+from torch import nn
+from torch.utils.data import DataLoader
+
+from src.data.baseline import ImgurDataset
+from src.data.wrappers import ChannelShuffleImage, Resize, NormalizeImages, GetRandomText, DrawTextCache
+from src.losses import VGGPerceptualLoss
+from src.losses.ocr2 import OCRV2Loss
+from src.losses.utils import LossScaler
+from src.metrics.ocr import ImageCharErrorRate
+from src.models.rfdn import RFDN
+from src.pipelines.simplest import SimplestEditingViz
+
+
+class SimplestGenerator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.backbone = RFDN(6, upscale=1)
+
+    def forward(self, style, content):
+        inputs = torch.concat([style, content], dim=1)
+        res = self.backbone(inputs)
+        return res
+
+
+class Config:
+    mean = (0.485, 0.456, 0.406)
+    std = (0.229, 0.224, 0.225)
+    size = (192, 64)
+    crops_path = Path("/cache/data/imgur/crops")
+
+    batch_size = 32
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    def __init__(self):
+        generator = SimplestGenerator().to(self.device)
+        optimizer = torch.optim.Adam(generator.parameters(), lr=2e-4)
+
+        trainset = self.get_dataset(self.crops_path / 'train')
+        valset = self.get_dataset(self.crops_path / 'val')
+
+        self.trainloader = DataLoader(trainset, batch_size=self.batch_size, shuffle=True)
+        self.valloader = DataLoader(valset, batch_size=self.batch_size)
+
+        ocr = OCRV2Loss(self.mean, self.std).to(self.device)
+        perc = VGGPerceptualLoss(self.mean, self.std, feature_layers=(), style_layers=(0, 1, 2, 3))
+        perc = LossScaler(perc.to(self.device), 0.002)
+
+        criterions = [
+            {'criterion': ocr, 'name': 'ocr', 'pred_key': 'pred_base', 'target_key': 'random'},
+            {'criterion': perc, 'name': 'perc', 'pred_key': 'pred_base', 'target_key': 'image'},
+        ]
+
+        cer = ImageCharErrorRate(self.mean, self.std).to(self.device)
+        psnr = torchmetrics.image.PeakSignalNoiseRatio().to(self.device)
+
+        metrics = [
+            {'metric': cer, 'name': 'cer', 'pred_key': 'pred_base', 'target_key': 'random'},
+            {'metric': psnr, 'name': 'psnr', 'pred_key': 'pred_original', 'target_key': 'image'},
+        ]
+
+        self.pipeline = SimplestEditingViz(
+            generator=generator,
+            optimizer=optimizer,
+            criterions=criterions,
+            metrics=metrics,
+            style_key='image',
+            draw_orig='draw_orig',
+            text_orig='content',
+            draw_rand='draw_random',
+            text_rand='random',
+        )
+
+        self.trainer = Trainer(accelerator=self.device, max_epochs=10)
+
+    def get_dataset(self, root):
+        dataset = ImgurDataset(root)
+        dataset = ChannelShuffleImage(dataset, 'image')
+
+        dataset = GetRandomText(dataset, root, 'random')
+        draw_cache = "/cache/data/cache/draw"
+        dataset = DrawTextCache(dataset, 'content', 'draw_orig', draw_cache)
+        dataset = DrawTextCache(dataset, 'random', 'draw_random', draw_cache)
+
+        dataset = Resize(dataset, 'image', self.size)
+        dataset = Resize(dataset, 'draw_orig', self.size, interpolation=cv2.INTER_NEAREST)
+        dataset = Resize(dataset, 'draw_random', self.size, interpolation=cv2.INTER_NEAREST)
+
+        dataset = NormalizeImages(dataset, ['image', 'draw_orig', 'draw_random'], self.mean, self.std)
+        return dataset
+
+    def run(self):
+        self.trainer.fit(self.pipeline, self.trainloader, self.valloader)
--- src/losses/typeface_perceptual.py
+++ src/losses/typeface_perceptual.py
@@ -1,14 +1,19 @@
-from loguru import logger
+from pathlib import Path
+
+import kornia
 import torch
 import torchvision
-import torchvision.transforms as T
+from kornia.enhance import Denormalize, Normalize
+from loguru import logger
+
 from src.disk import disk
-from pathlib import Path
 
 
 class TypefacePerceptualLoss(torch.nn.Module):
     def __init__(
             self,
+            mean,
+            std,
             model_remote_path='models/TypefaceClassifier/model',
             model_local_path='models/TypefaceClassifier/model'):
         super().__init__()
@@ -16,8 +21,9 @@ class TypefacePerceptualLoss(torch.nn.Module):
             if not disk.get_disabled():
                 disk.download(model_remote_path, model_local_path)
             else:
-                logger.error('You need to download the TypefaceClassifier/model from https://disk.yandex.ru/d/gTJa6Bg2QW0GJQ and '
-                             'put it in the models/ folder in the root of the repository')
+                logger.error(
+                    'You need to download the TypefaceClassifier/model from https://disk.yandex.ru/d/gTJa6Bg2QW0GJQ and '
+                    'put it in the models/ folder in the root of the repository')
                 exit(1)
 
         model = torchvision.models.vgg16().cuda()
@@ -28,10 +34,17 @@ class TypefacePerceptualLoss(torch.nn.Module):
         for p in model.parameters():
             p.requires_grad = False
 
-        self.transform = T.Compose([
-            T.Resize((64, 192)),
-            T.Normalize([0.4803, 0.4481, 0.3976], [0.2769, 0.2690, 0.2820]),
-        ])
+        self.size = (224, 224)
+        self.norm = Normalize(mean=[0.4803, 0.4481, 0.3976], std=[0.2769, 0.2690, 0.2820])
+        self.denorm = Denormalize(torch.Tensor(mean), torch.Tensor(std))
+
+    def prepare_sample(self, images):
+        images = self.denorm(images)
+        images = self.norm(images)
+        images = kornia.geometry.transform.resize(images, self.size, interpolation=self.interpolation)
+        return images
 
     def forward(self, inputs, targets):
-        return torch.nn.functional.l1_loss(self.model(inputs), self.model(targets))
+        inputs = self.model(self.prepare_sample(inputs))
+        targets = self.model(self.prepare_sample(targets))
+        return torch.nn.functional.l1_loss(inputs, targets)
