4d8c0e3f71e4703210734ceef56874ee4e0982a6
Renat Khizbullin <renat.khizbullin@incode.com>
2023-10-12 11:25:59 +0200

    Add gan sch

--- src/utils/warmup.py
+++ src/utils/warmup.py
@@ -4,11 +4,16 @@ from torch.optim.lr_scheduler import LambdaLR
 from torch.optim.optimizer import Optimizer
 
 
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
 class WarmupScheduler(LambdaLR):
     def __init__(self, optimizer: Optimizer, num_steps: int, last_epoch: int = -1):
         self.num_steps = num_steps
         super().__init__(optimizer, lambda x: min(x / self.num_steps, 1.0), last_epoch=last_epoch)
 
     def step(self, epoch: Optional[int] = ...) -> None:
-        print(f'scheduler step: {epoch}, {self.get_lr()}')
+        print(f'scheduler step: {epoch}, {get_lr(self.optimizer)}')
         super().step(epoch)
