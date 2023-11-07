56c2d1b1f47ccb9164c17effca4429014c940568
Renat Khizbullin <renat.khizbullin@incode.com>
2023-11-07 14:05:49 +0100

    stylegan auto

--- src/pipelines/gan.py
+++ src/pipelines/gan.py
@@ -171,17 +171,20 @@ class SimpleGAN(pl.LightningModule):
             metric.update(predictions[pred_key], predictions[target_key])
 
         if batch_idx == 0:
-            recogs_base = self.ocr.recognize(predictions['pred_original'])
+            if self.calc_orig:
+                recogs_base = self.ocr.recognize(predictions['pred_original'])
             recogs_rand = self.ocr.recognize(predictions['pred_random'])
             for i in range(10):
                 self.visualize_image(f'val_{i}/image', predictions[self.style_key][i])
                 self.visualize_image(f'val_{i}/pred_random', predictions['pred_random'][i])
-                self.visualize_image(f'val_{i}/pred_original', predictions['pred_original'][i])
+                if self.calc_orig:
+                    self.visualize_image(f'val_{i}/pred_original', predictions['pred_original'][i])
 
                 self.visualize_image(f'val_{i}/draw_orig', predictions[self.draw_orig][i])
                 self.visualize_image(f'val_{i}/draw_rand', predictions[self.draw_rand][i])
 
-                self.visualize_image(f'val_{i}/recog_orig', draw_word(recogs_base[i]))
+                if self.calc_orig:
+                    self.visualize_image(f'val_{i}/recog_orig', draw_word(recogs_base[i]))
                 self.visualize_image(f'val_{i}/recog_rand', draw_word(recogs_rand[i]))
 
     def validation_epoch_end(self, outputs) -> None:
